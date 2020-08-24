c5533f13c557550ea2d91497f157d86a11f2c9c1
Daianes26 <daianeasilva26@gmail.com>
2020-08-23 22:36:21 -0400

    Add countdown functionality

--- script.js
+++ script.js
@@ -83,6 +83,27 @@ function createEquations() {
 //   itemContainer.appendChild(bottomSpacer);
 // }
 
+
+//Display the countdown
+function countdownStart(){
+  countdown.textContent = '3';
+  setTimeout(() => {
+    countdown.textContent = '2';
+  }, 1000);
+  setTimeout(() => {
+    countdown.textContent = '1';
+  }, 2000);
+  setTimeout(() => {
+    countdown.textContent = 'GO!';
+  }, 3000);
+}
+// Navigate from Splash Page to Countdown Page
+function showCountdown(){
+  countdownPage.hidden = false;
+  splashPage.hidden = true;
+  countdownStart()
+}
+
 //Get value from selected radio button
 function getRadioValue(){
   let radioValue;
@@ -99,6 +120,11 @@ function selectQuestionAmount(e){
   e.preventDefault();
   questionAmount = getRadioValue();
   console.log('question amount:', questionAmount);
+  if (questionAmount) {
+    showCountdown();
+  } else {
+    alert('Please select an amount!');
+  }
 }
 
 //Event Listeners
